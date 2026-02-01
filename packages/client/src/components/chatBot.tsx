d5da758769416bb32251c683fc0e18f84c70e8aa
nadmez <nadmez@yahoo.com>
2026-02-01 17:52:24 +0300

    Chatbox component enhancements

--- packages/client/src/components/chatBot.tsx
+++ packages/client/src/components/chatBot.tsx
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import ReactMarkdown from 'react-markdown';
+import { useTranslation } from 'react-i18next';
 
 import { Button } from './ui/button';
+import BotTypingSpinner from './botTypingSpinner';
 import { FaArrowUp } from 'react-icons/fa';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 type FormData = {
    prompt: string;
@@ -22,21 +24,43 @@ type Message = {
 const ChatBot = () => {
    const [messages, setMessages] = useState<Message[]>([]);
    const conversationId = useRef<string | undefined>(undefined);
+   const [isBotTyping, setIsBotTyping] = useState(false);
+   const [error, setError] = useState<string | null>(null);
+   const messageContainerRef = useRef<HTMLDivElement | null>(null);
 
    const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
+   useEffect(() => {
+      const el = messageContainerRef.current;
+      if (!el) return;
+      el.scrollTo({ top: el.scrollHeight, behavior: 'smooth' });
+   }, [messages]);
+
    const onSubmitHandler = async ({ prompt }: FormData) => {
-      setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
-      reset();
+      try {
+         setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
+         setIsBotTyping(true);
+         reset({ prompt: '' });
+         setError(null);
 
-      if (!conversationId.current) conversationId.current = crypto.randomUUID();
+         if (!conversationId.current)
+            conversationId.current = crypto.randomUUID();
 
-      const { data } = await axios.post<ChatResponse>('/api/chat', {
-         prompt,
-         conversationId: conversationId?.current,
-      });
+         const { data } = await axios.post<ChatResponse>('/api/chat', {
+            prompt,
+            conversationId: conversationId?.current,
+         });
 
-      setMessages((prev) => [...prev, { content: data.message, role: 'bot' }]);
+         setMessages((prev) => [
+            ...prev,
+            { content: data.message, role: 'bot' },
+         ]);
+      } catch (error) {
+         console.error(error);
+         setError('Something went wrong! Please try again later.');
+      } finally {
+         setIsBotTyping(false);
+      }
    };
 
    const onKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
@@ -46,12 +70,24 @@ const ChatBot = () => {
       }
    };
 
+   const onCopyMessage = (e: React.ClipboardEvent) => {
+      const selection = window.getSelection()?.toString().trim();
+      if (selection) {
+         e.preventDefault();
+         e.clipboardData.setData('text/plain', selection);
+      }
+   };
+
    return (
-      <div className="chat-container flex flex-col gap-3">
-         <div className="message-container flex flex-col gap-2 max-h-[70vh] overflow-y-auto p-4 border-2 rounded-3xl">
+      <div className="chat-container flex flex-col gap-3 h-screen">
+         <div
+            ref={messageContainerRef}
+            className="message-container flex flex-col flex-1 gap-2 max-h-[75vh] overflow-y-auto p-4 border-2 rounded-3xl"
+         >
             {messages.map((msg, index) => (
-               <p
+               <div
                   key={index}
+                  onCopy={onCopyMessage}
                   className={`px-3 py-1 rounded-xl ${
                      msg.role === 'user'
                         ? 'self-end bg-blue-400 text-white'
@@ -59,8 +95,11 @@ const ChatBot = () => {
                   }`}
                >
                   <ReactMarkdown>{msg.content}</ReactMarkdown>
-               </p>
+               </div>
             ))}
+
+            {isBotTyping && <BotTypingSpinner />}
+            {error && <div className="text-red-500">{error}</div>}
          </div>
          <form
             className="prompt-form flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
@@ -78,6 +117,7 @@ const ChatBot = () => {
                className="w-full border-0 focus:outline-0 resize-none"
                maxLength={1000}
                onKeyDown={onKeyDown}
+               autoFocus
             ></textarea>
             <Button
                disabled={!formState.isValid}
